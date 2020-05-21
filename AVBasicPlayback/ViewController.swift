b8dd9745db90d24f9c728526296b32207ad79ec0
Akin Williams <aowilliams@arstropica.com>
2020-05-21 09:44:20 -0400

    Initial Commit

---
+++ AVBasicPlayback/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVBasicPlayback
+//
+//  Created by Admin on 5/21/20.
+//  Copyright © 2020 ArsTropica LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
